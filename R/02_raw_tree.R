931703012184877e531bdb93113b0eefcfbad11e
Alex Barth <abarth1014@gmail.com>
2023-10-04 21:25:29 -0400

    fix 0's issue

--- R/02_raw_tree.R
+++ R/02_raw_tree.R
@@ -39,6 +39,17 @@ tree_sp_riv_5m <- tree_raw |>
   summarize(riv = riv_calc(total_area),
             tree_id = Tree_id)
 
+temp_df <- data.frame(
+  transect_id = rep(rep(1:3, each = length(seq(0,35,5))), each = length(unique(tree_sp_riv_5m$tree_id))),
+  bin = rep(rep(seq(0,35,5), times = 3), each = length(unique(tree_sp_riv_5m$tree_id)))
+)
+temp_df$tree_id <- rep(unique(tree_sp_riv_5m$tree_id), times = length(seq(0,35,5)) * 3)
+
+tree_sp_riv_5m <- temp_df |> 
+  left_join(tree_sp_riv_5m)
+
+tree_sp_riv_5m$riv[is.na(tree_sp_riv_5m$riv)] <- 0
+
 write.csv(tree_sp_riv_5m, './data/tree_sp_riv_5m.csv',
           row.names = F)
 
@@ -48,6 +59,19 @@ tree_cat_riv_5m <- tree_raw |>
   summarize(total_area = sum(area)) |> 
   summarize(riv = riv_calc(total_area),
             Tree_cat = Tree_cat)
+
+temp_df <- data.frame(
+  transect_id = rep(rep(1:3, each = length(seq(0,35,5))), each = length(unique(tree_cat_riv_5m$Tree_cat))),
+  bin = rep(rep(seq(0,35,5), times = 3), each = length(unique(tree_cat_riv_5m$Tree_cat)))
+)
+temp_df$Tree_cat <- rep(unique(tree_cat_riv_5m$Tree_cat), times = length(seq(0,35,5)) * 3)
+
+tree_cat_riv_5m <- temp_df |> 
+  left_join(tree_cat_riv_5m)
+
+tree_cat_riv_5m$riv[is.na(tree_cat_riv_5m$riv)] <- 0
+
+
 
 write.csv(tree_cat_riv_5m, './data/tree_cat_5m.csv',
           row.names = F)
@@ -60,6 +84,21 @@ tree_sp_riv_subregion <- tree_raw |>
   summarize(riv = riv_calc(total_area),
             tree_id = Tree_id)
 
+
+temp_df <- data.frame(
+  transect_id = rep(rep(1:3, each = length(unique(tree_sp_riv_subregion$Subregion))),
+                    each = length(unique(tree_sp_riv_subregion$tree_id))),
+  Subregion = rep(rep(unique(tree_sp_riv_subregion$Subregion), times = 3), 
+            each = length(unique(tree_sp_riv_subregion$tree_id)))
+)
+temp_df$tree_id <- rep(unique(tree_sp_riv_subregion$tree_id), times = 9)
+
+tree_sp_riv_subregion <- temp_df |> 
+  left_join(tree_sp_riv_subregion)
+
+tree_sp_riv_subregion$riv[is.na(tree_sp_riv_subregion$riv)] <- 0
+
+
 write.csv(tree_sp_riv_subregion, './data/tree_sp_subregion.csv',
           row.names = F)
 
@@ -69,6 +108,21 @@ tree_cat_riv_subregion <- tree_raw |>
   summarize(total_area = sum(area)) |> 
   summarize(riv = riv_calc(total_area),
             Tree_cat = Tree_cat)
+
+
+temp_df <- data.frame(
+  transect_id = rep(rep(1:3, each = length(unique(tree_cat_riv_subregion$Subregion))),
+                    each = length(unique(tree_cat_riv_subregion$Tree_cat))),
+  Subregion = rep(rep(unique(tree_cat_riv_subregion$Subregion), times = 3), 
+                  each = length(unique(tree_cat_riv_subregion$Tree_cat)))
+)
+temp_df$Tree_cat <- rep(unique(tree_cat_riv_subregion$Tree_cat), times = 9)
+
+tree_cat_riv_subregion <- temp_df |> 
+  left_join(tree_cat_riv_subregion)
+
+tree_cat_riv_subregion$riv[is.na(tree_cat_riv_subregion$riv)] <- 0
+
 
 write.csv(tree_cat_riv_subregion, './data/tree_cat_subregion.csv',
           row.names = F)
